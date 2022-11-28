95d9d0d49790e49363636615122bd418de04da40
Kyle Andrus <kyleandrus3@gmail.com>
2022-11-28 14:23:57 -0700

    add text to elements

--- src/index.js
+++ src/index.js
@@ -49,6 +49,41 @@ const ctaImage = document.querySelector('#cta-img');
 const textContentHeadlines = document.querySelectorAll('.text-content h4');
 const textContentParas = document.querySelectorAll('.text-content p');
 const middleImage = document.querySelector('#middle-img');
-const contactInfo = document.querySelector('.contact');
+const contactInfoHead = document.querySelector('.contact h4');
+const contactInfoPara = document.querySelectorAll('.contact p');
 const footerA = document.querySelector('footer a');
+
+navLinks[0].textContent = "Services";
+navLinks[1].textContent = "Product";
+navLinks[2].textContent = "Vision";
+navLinks[3].textContent = "Features";
+navLinks[4].textContent = "About";
+navLinks[5].textContent = "Contact";
+headline.textContent = "DOM Is awesome"
+button1.textContent = "Get Started";
+textContentHeadlines[0].textContent = "Features";
+textContentHeadlines[1].textContent = "About";
+textContentHeadlines[2].textContent = "Services";
+textContentHeadlines[3].textContent = "Product";
+textContentHeadlines[4].textContent = "Vision"
+textContentParas[0].textContent = `Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio,
+in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus
+scelerisque quis.`;
+textContentParas[1].textContent = `About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio,
+in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus
+scelerisque quis.`;
+textContentParas[2].textContent = `Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio,
+in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus
+scelerisque quis.`;
+textContentParas[3].textContent = `Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio,
+in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus
+scelerisque quis.`;
+textContentParas[4].textContent = `Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio,
+in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus
+scelerisque quis.`;
+
+contactInfoHead.textContent = "Contact";
+contactInfoPara[0].textContent = "123 Way 456 Street Somewhere, USA";
+contactInfoPara[1].textContent = "1 (888) 888-8888";
+contactInfoPara[2].textContent = "sales@greatidea.io";
 
